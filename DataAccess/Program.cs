8be09312fa367ff3fc5525f3ec6bab0e85bfc3dd
VictorSardellaro <victor.henryk@outlook.com>
2021-07-07 14:38:40 -0300

    Initial

---
+++ DataAccess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost, 1433; Database=baltaio;User ID=sa;Password=1q2w3e4r@#$";
+            // Microsft.Data.SqlClient (Nuget)
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
