c7e739f1a52b02e44fbfeb8f7ee40304ea4d380e
VictorSardellaro <victor.henryk@outlook.com>
2021-07-07 16:27:55 -0300

    Microsoft.Data.SqlClient

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace DataAccess
 {
@@ -7,8 +8,28 @@ namespace DataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost, 1433; Database=baltaio;User ID=sa;Password=1q2w3e4r@#$";
-            // Microsft.Data.SqlClient (Nuget)
-            Console.WriteLine("Hello World!");
+            // Microsoft.Data.SqlClient (Nuget)
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                System.Console.WriteLine("Conectado");
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        System.Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+
+                }
+            }
+            // Conecta e encerra a conexão com o Banco ter o risco dela ficar aberta
+
         }
     }
 }
