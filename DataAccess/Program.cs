1ce7fe9b2406975cf1271c4746fca2bb8b8f425a
VictorSardellaro <victor.henryk@outlook.com>
2021-07-08 11:01:27 -0300

    SQL_Injection

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -14,12 +14,36 @@ namespace DataAccess
             const string connectionString = "Server=localhost, 1433; Database=baltaio;User ID=sa;Password=1q2w3e4r@#$";
             // Microsoft.Data.SqlClient (Nuget)
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            //SQL Injection
+
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                values(
+                    id, 
+                    title, 
+                    url, 
+                    summary, 
+                    order, 
+                    description, 
+                    featured)";
+
+
             using (var connection = new SqlConnection(connectionString))
             {
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                foreach (var item in categories)
                 {
-                    System.Console.WriteLine($"{category.Id} - {category.Title}");
+                    System.Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
 
