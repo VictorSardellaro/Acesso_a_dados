37320a6995ff561e6f633a14bced119919989f0a
VictorSardellaro <victor.henryk@outlook.com>
2021-07-12 18:04:53 -0300

    Transaction

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -34,7 +34,9 @@ namespace DataAccess
                 //OneToMany(connection);
                 //QueryMultiple(connection);
                 //SelectIn(connection);
-                Like(connection, "backend");
+                //Like(connection, "backend");
+                Transaction(connection);
+
 
             }
 
@@ -389,6 +391,49 @@ namespace DataAccess
             }
         }
 
+
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "00000000000";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }, transaction);
+
+                //transaction.Commit();
+                transaction.Rollback();
+
+                Console.WriteLine($"{rows} linhas inseridas");
+            }
+        }
     }
 }
 
